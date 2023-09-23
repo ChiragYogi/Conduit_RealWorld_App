4f9b7339baeb4e0a5f05ac868ad920cfa1395466
ChiragYogi <chirag433@gmail.com>
2023-09-23 17:38:03 +0530

    initial commit.

---
+++ app/src/main/java/com/example/conduitrealworldapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.conduitrealworldapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
