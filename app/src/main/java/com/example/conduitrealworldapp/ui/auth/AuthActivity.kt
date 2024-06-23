35e7a23f4cc3694e46c46f09755360519f51560c
ChiragYogi <chirag433@gmail.com>
2024-06-23 20:01:27 +0530

    Auth Activity added for auth module

---
+++ app/src/main/java/com/example/conduitrealworldapp/ui/auth/AuthActivity.kt
@@ -0,0 +1,23 @@
+package com.example.conduitrealworldapp.ui.auth
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.conduitrealworldapp.ui.theme.AppTheme
+
+class AuthActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AppTheme {
+                val navController = rememberNavController()
+                SignInScreen(modifier = Modifier, navController = navController)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/conduitrealworldapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/conduitrealworldapp/ui/theme/Theme.kt
@@ -94,11 +94,12 @@ fun AppTheme(
         useDarkTheme -> DarkColors
         else -> LightColors
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.background.toArgb() // here change the color
+            window.statusBarColor = colorScheme.primary.toArgb() // here change the color
             window.navigationBarColor = colorScheme.background.toArgb() // here change the color
         }
     }
